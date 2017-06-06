a4177f56c5cec313097e58a5fb725ab58ab24ce0
Rohit Dabra <rohitdabra@Rohits-MacBook-Pro.local>
2017-06-06 11:49:22 +0530

    arabic support

--- CareerCoach/LanguageService.swift
+++ CareerCoach/LanguageService.swift
@@ -22,7 +22,7 @@ class LanguageService
         languages.append(Language(languageId: 5, languageName: "Français", culture: "fr"))
         languages.append(Language(languageId: 6, languageName: "Español", culture: "es"))
         languages.append(Language(languageId: 7, languageName: "Português", culture: "pt-PT"))
-        //languages.append(Language(languageId: 10, languageName: "العربية", culture: "ar"))
+        languages.append(Language(languageId: 10, languageName: "العربية", culture: "ar"))
         languages.append(Language(languageId: 41, languageName: "Polish", culture: "pl"))
         languages.append(Language(languageId: 43, languageName: "Русский", culture: "ru"))
         languages.append(Language(languageId: 14, languageName: "Chinese (Simplified)", culture: "zh-Hans"))
